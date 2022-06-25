b0b4f8cce584a2c8e7ad275ac1588789a32a21af
Vinicius Oliveira de Freitas <mac@macs-MacBook-Air.local>
2022-06-25 20:43:55 -0300

    App

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-  uri: "https://api-us-west-2.graphcms.com/v2/cl4t3ngm82w9u01z13p8xeliu/master",
+  uri: import.meta.env.VITE_API_URL,
+  headers: {
+    Authorization: `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`,
+  },
   cache: new InMemoryCache(),
 });
